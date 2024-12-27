c88b1cd11d3b0939b02bb273e27e363c5137bb4e
Raph Levien <raph@google.com>
2024-12-26 16:38:17 -0800

    Delete unused stuff
    We'll very likely bring lots of it back (especially Neon fp16 stuff), but the right way to do that is to fish it out of the `next` branch.

--- src/base/f16.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 the Fearless_SIMD Authors
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-
-use crate::{f16, mask16x4, mask16x8, u8x8, u8x16};
-
-impl_simd!(f16x4, f16, 4, mask16x4, u8x8);
-impl_simd!(f16x8, f16, 8, mask16x8, u8x16);
--- src/lib.rs
+++ src/lib.rs
@@ -26,23 +26,3 @@ pub use base::*;
 // TODO: be more consistent, either bring avx2 items to crate level
 // or make fallback a module too.
 pub use fallback::*;
-
-/*
-mod detect;
-
-pub use detect::*;
-
-// For now, only bring in f16 on aarch64. We can also bring it in
-// on x86_64, but only Sapphire Rapids supports it.
-
-#[cfg(all(target_arch = "aarch64", feature = "half"))]
-pub type f16 = half::f16;
-#[cfg(all(target_arch = "aarch64", not(feature = "half")))]
-mod half_assed;
-#[cfg(all(target_arch = "aarch64", not(feature = "half")))]
-pub use half_assed::f16;
-
-*/
-
-// Experimental
-// pub mod token;
